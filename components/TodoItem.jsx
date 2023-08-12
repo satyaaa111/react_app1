580cb4a78cb1050f69494e9291227ce2b3a9d238
satyaaa111 <129730891+satyaaa111@users.noreply.github.com>
2023-08-12 22:12:14 +0530

    Add files via upload
    Files are added for the first time

---
+++ components/TodoItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+
+function TodoItem(props){
+
+  const[isDone, setIsDone] = React.useState(false);
+
+  function handleClick(){
+      setIsDone((prevValue) => {
+        return !prevValue ;
+      })
+  }
+  return ( 
+    <div onClick={handleClick}>
+        <li  style={{textDecoration:
+        isDone? "line-through": "none"}}>{props.text}</li>
+    </div> 
+      
+    
+  );
+}
+
+export default TodoItem;
